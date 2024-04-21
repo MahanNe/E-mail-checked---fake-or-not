1b1f44449947bb03f22ce5442682dc417b3bcda8
MahanNe <159655797+MahanNe@users.noreply.github.com>
2024-04-21 09:02:40 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const express = require('express');
 const emailDomainCheck = require('email-domain-check');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Middleware to parse JSON requests
 app.use(express.json());
 
-// API route for checking email domain
+// Endpoint to check if the email domain is invalid or disposable
 app.get('/', async (req, res) => {
   const email = req.query.email;
 
@@ -19,6 +19,7 @@ app.get('/', async (req, res) => {
     const result = await emailDomainCheck(email);
     res.json({ isValid: result.isValid, isDisposable: result.isDisposable });
   } catch (error) {
+    console.error('Error checking email domain:', error);
     res.status(500).json({ error: 'An error occurred while checking the email domain.' });
   }
 });
